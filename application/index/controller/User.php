0a8a41d9fce85a5692e208ba85e5525c7d0a7b04
zhaoxiang <756958008@qq.com>
2016-10-31 15:02:35 +0800

    init app

---
+++ application/index/controller/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * @since   2016-02-18
+ * @author  zhaoxiang <zhaoxiang051405@outlook.com>
+ */
+
+namespace app\index\controller;
+use \think\Controller;
+
+class User extends Controller {
+    public function index(){
+        echo 11111;
+    }
+}
