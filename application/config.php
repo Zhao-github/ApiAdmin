ecb66d3675f529f76261abc3d96dc5b212afb63c
zhaoxiang <756958008@qq.com>
2016-11-01 10:57:32 +0800

    进一步调整目录结构以应对多域名方案！

--- application/config.php
+++ application/config.php
@@ -158,7 +158,9 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__STATIC__'=>'/public/static/'
+        '__STATIC__'=>'/public/static/',
+        '__INDEX__'=>'/public/static/index/',
+        '__ADMIN__'=>'/public/static/admin/',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
--- public/admin/index.php
+++ public/admin/index.php
@@ -12,6 +12,6 @@
 // [ 应用入口文件 ]
 define('BIND_MODULE','admin');
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/../../application/');
 // 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+require __DIR__ . '/../../thinkphp/start.php';
