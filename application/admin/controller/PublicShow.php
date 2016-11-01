648adec06a28d7378f705d97761e46cb7fc573eb
zhaoxiang <756958008@qq.com>
2016-11-01 10:32:14 +0800

    新增用户登录的方法

---
+++ application/admin/controller/PublicShow.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @since   2016-02-18
+ * @author  zhaoxiang <zhaoxiang051405@outlook.com>
+ */
+
+namespace app\admin\controller;
+
+
+class PublicShow extends Base {
+
+    public function show_404(){
+        $this->fetch('public/404');
+    }
+
+    public function show_500(){
+        $this->fetch('public/500');
+    }
+
+    public function showBreadcrumb(){
+
+    }
+
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ *
+ * @since   2016-02-18
+ * @author  zhaoxiang <zhaoxiang051405@outlook.com>
+ */
 namespace app\index\controller;
 
 class Index extends Base {
