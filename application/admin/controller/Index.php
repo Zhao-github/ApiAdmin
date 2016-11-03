a0c554194dac361bfeebf30bd24c2fa0c54bb7be
zhaoxiang <756958008@qq.com>
2016-11-03 14:15:24 +0800

    新增模版引擎文件

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -3,6 +3,7 @@ namespace app\admin\controller;
 
 class Index extends Base  {
     public function index() {
-        return (new PublicShow())->show_404();
+        $this->assign('title', '首页');
+        return $this->fetch();
     }
 }
--- public/static/admin/dist/js/template.js
+++ public/static/admin/dist/js/template.js
@@ -4,13 +4,30 @@
  * You should not use this file in production.
  * This file is for demo purposes only.
  */
-(function ($) {
+$.AdminTemplate = {};
+/**
+ * 格式化时间戳（为了和PHP的date函数统一，这里的时间戳都是10位，不包含毫秒）
+ * @param timestamp
+ * @returns {string}
+ */
+var formatDate = function ( timestamp ) {
+    timestamp *= 1000;
+    var date = new Date(timestamp);
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    var D = (date.getDate()+1 < 10 ? '0'+(date.getDate()+1) : date.getDate()+1) + ' ';
+    var h = date.getHours() + ':';
+    var m = date.getMinutes() + ':';
+    var s = date.getSeconds();
+    return Y+M+D+h+m+s;
+};
+(function ($, AdminTemplate) {
 
     "use strict";
 
-    function create( jsonStr ) {
+    AdminTemplate.buildDom = function ( jsonStr ) {
 
-    }
+    };
 
     function emptyList() {
 
@@ -28,7 +45,7 @@
 
     }
 
-    function easyForm() {
+    function easyForm( formObj ) {
 
     }
 
@@ -44,21 +61,12 @@
 
     }
 
-    /**
-     * 格式化时间戳（为了和PHP的date函数统一，这里的时间戳都是10位，不包含毫秒）
-     * @param timestamp
-     * @returns {string}
-     */
-    function formatDate( timestamp ) {
-        timestamp *= 1000;
-        var date = new Date(timestamp);
-        var Y = date.getFullYear() + '-';
-        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-        var D = (date.getDate()+1 < 10 ? '0'+(date.getDate()+1) : date.getDate()+1) + ' ';
-        var h = date.getHours() + ':';
-        var m = date.getMinutes() + ':';
-        var s = date.getSeconds();
-        return Y+M+D+h+m+s;
+    function breadcrumb(  ) {
+
     }
 
-})(jQuery);
+    AdminTemplate.a = function () {
+
+    }
+
+})(jQuery, $.AdminTemplate);
