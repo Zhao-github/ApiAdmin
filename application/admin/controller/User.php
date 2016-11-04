30775cbe647bb4ee11ef54112936ea717539ca4a
zhaoxiang <756958008@qq.com>
2016-11-05 00:12:22 +0800

    修改数据库配置

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -21,11 +21,38 @@ class User extends Base {
                 return $this->error('缺少关键数据！');
             }
             $password = $this->getPwdHash($password);
-            $isOk = Db::table('users')->where(['username' => $username, 'password' => $password])->count();
-            if( !$isOk ){
+            $userInfo = Db::table('user')->where(['username' => $username, 'password' => $password])->find();
+            if( empty($userInfo) ){
                 $this->error('用户名或者密码错误！');
             }else{
+                if( $userInfo['status'] ){
 
+                    //保存用户信息和登录凭证
+                    S($userInfo['_id'], session_id(), C('ONLINE_TIME'));
+                    session('uid', $userInfo['_id']);
+
+                    //获取跳转链接，做到从哪来到哪去
+                    $url = empty(session('loginFrom')) ? U('Index/index') : session('loginFrom');
+                    session('loginFrom', null);
+
+                    //更新用户数据
+                    $userData = D('UserData')->where(['uid' => $userInfo['_id']])->find();
+                    $data = [];
+                    if( $userData ){
+                        $data['loginTimes'] = $userData['loginTimes'] + 1;
+                        $data['lastLoginIp'] = get_client_ip(1);
+                        $data['lastLoginTime'] = NOW_TIME;
+                        D('UserData')->where(['uid' => $userInfo['_id']])->save($data);
+                    }else{
+                        $data['loginTimes'] = 1;
+                        $data['uid'] = $userInfo['_id'];
+                        D('UserData')->add($data);
+                    }
+
+                    $this->success('登录成功', $url);
+                }else{
+                    $this->error('用户已被封禁，请联系管理员');
+                }
             }
         }else{
             return $this->fetch();
--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => '',
+    'database'       => 'admin',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => '',
+    'password'       => 'root',
     // 端口
     'hostport'       => '',
     // 连接dsn
