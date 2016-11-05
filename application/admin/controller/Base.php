cca948479933f296181f9827e991678f59d5ad9c
zhaoxiang <756958008@qq.com>
2016-11-06 02:13:45 +0800

    modified 优化404页面显示 added 空方法自动跳转404

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -11,8 +11,26 @@ use think\Controller;
 class Base extends Controller {
 
     public $primaryKey;
+    public $uid;
 
     public function _initialize(){
         $this->primaryKey = config('SQL_PRIMARY_KEY');
+
+        //初始化系统
+        $this->uid = session('uid');
+        $this->assign('uid',$this->uid);
+//        $this->iniSystem();
+
+        //控制器初始化
+        if(method_exists($this,'_myInitialize')){
+            $this->_myInitialize();
+        }
+    }
+
+    /**
+     * 空方法默认的页面
+     */
+    public function _empty(){
+        return (new PublicShow())->show_404();
     }
 }
