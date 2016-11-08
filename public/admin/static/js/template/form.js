1cccbd41f82b41b20d6e6f29ca0bb13516076e0c
zhaoxiang <756958008@qq.com>
2016-11-08 18:26:04 +0800

    added 新增FormJS的引入

--- public/admin/static/js/template/form.js
+++ public/admin/static/js/template/form.js
@@ -1,4 +1,40 @@
 /**
  * Created by 7d-vision on 2016/11/7.
  */
-(function ($) {})(jQuery);
+(function ($) {
+
+    $.buildForm = function ( formObj ) {
+        var formHtml = '<div class="box" id="formBox"><div class="box-body">';
+
+        formHtml += '</div></div>';
+        return formHtml;
+    };
+
+    function buildInput( inputObj ) {
+
+    }
+
+    function buildButton( buttonObj ) {
+
+    }
+    
+    function buildTextarea() {
+        
+    }
+    
+    function buildSelect() {
+        
+    }
+    
+    function buildEditors() {
+        
+    }
+
+    function buildUpload() {
+
+    }
+
+    function buildCode() {
+
+    }
+})(jQuery);
