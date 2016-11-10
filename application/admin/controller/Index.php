29448e0e9cfa24e29ed8a8fdbba0b3ca91154bf1
zhaoxiang <756958008@qq.com>
2016-11-10 16:51:48 +0800

    added 菜单动态生成

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,9 +1,21 @@
 <?php
 namespace app\admin\controller;
 
+use app\admin\model\Menu;
+
 class Index extends Base  {
     public function index() {
+        $dataObj = Menu::all(function($query){
+            $query->order('sort', 'asc');
+        });
+        foreach ($dataObj as $value){
+            if( !$value->hide ){
+                $data[] = $value->toArray();
+            }
+        }
+        $data = listToTree($data);
         $this->assign('title', '首页');
+        $this->assign('menuData', $data);
         return $this->fetch();
     }
 }
