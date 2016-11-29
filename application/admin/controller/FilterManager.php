ba6a930dc4da32f88278ce4950401b57c8ec10f4
zhaoxiang <756958008@qq.com>
2016-11-29 18:57:48 +0800

    added 新增加规则组相关函数

---
+++ application/admin/controller/FilterManager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 规则组配置
+ * @since   2016-02-18
+ * @author  zhaoxiang <zhaoxiang051405@outlook.com>
+ */
+
+namespace app\admin\controller;
+
+
+class FilterManager extends Base {
+
+}
