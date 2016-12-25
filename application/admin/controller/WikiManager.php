7beb37600d6d46a6e9dcc0a7512cfdf47109ffb8
zhaoxiang <756958008@qq.com>
2016-12-25 20:10:47 +0800

    added 文档控制

--- application/admin/controller/WikiManager.php
+++ application/admin/controller/WikiManager.php
@@ -10,4 +10,45 @@ namespace app\admin\controller;
 
 class WikiManager extends Base {
 
+    const FILE_CACHE = 0;
+    const SERVER_CACHE = 1;
+    const NO_CACHE = 2;
+
+    private $cacheType;
+
+    public function _myInitialize( $type = '' ) {
+        if( empty($type) ){
+            $this->cacheType = self::FILE_CACHE;
+        }else{
+            $this->cacheType = $type;
+        }
+    }
+
+    public function api(){
+
+    }
+
+    public function app(){
+
+    }
+
+    private function getWikiToken( $mark ){
+
+    }
+
+    private function getFileCache( $str ){
+
+    }
+
+    private function delFileCache( $str ){
+
+    }
+
+    private function getServerCache( $str ){
+
+    }
+
+    private function delServerCache( $str ){
+
+    }
 }
