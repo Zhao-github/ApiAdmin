509bde90b1e0ba530c1f1f631bf9410b48cdb57c
zhaoxiang <756958008@qq.com>
2017-06-23 16:24:32 +0800

    added 新增加文档表，用于处理Api文档的安全

--- Application/Data/apiAdmin.sql
+++ Application/Data/apiAdmin.sql
@@ -312,6 +312,7 @@ CREATE TABLE `api_document` (
 	`lastIp` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '最后访问IP',
 	`createTime` INT(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
 	`keep` INT(11) NOT NULL DEFAULT '0' COMMENT '有效时长（单位小时）',
+  `status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1生效，0失效',
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `key` (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文档访问秘钥';
