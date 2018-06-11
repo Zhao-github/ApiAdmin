2a720cb56a1299d1ff2dade4928fd8cdd73328df
zhaoxiang <zhaoxiang051405@gmail.com>
2018-06-11 09:28:27 +0800

    modified 优化代码格式

--- application/adminRoute.php
+++ application/adminRoute.php
@@ -1,4 +1,5 @@
 <?php
+
 use think\Route;
 
 $afterBehavior = [
@@ -10,17 +11,17 @@ $afterBehavior = [
 
 //一些带有特殊参数的路由写到这里
 Route::rule([
-    'admin/Login/index'  => [
+    'admin/Login/index'          => [
         'admin/Login/index',
-        ['method'=>'post']
+        ['method' => 'post']
     ],
-    'admin/Index/upload' => [
+    'admin/Index/upload'         => [
         'admin/Index/upload',
         [
             'method' => 'post', 'after_behavior' => ['\app\admin\behavior\ApiAuth', '\app\admin\behavior\AdminLog']
         ]
     ],
-    'admin/Login/logout' => [
+    'admin/Login/logout'         => [
         'admin/Login/logout',
         [
             'method' => 'get', 'after_behavior' => ['\app\admin\behavior\ApiAuth', '\app\admin\behavior\AdminLog']
@@ -30,245 +31,245 @@ Route::rule([
 
 
 //大部分控制器的路由都以分组的形式写到这里
-Route::group('admin',function(){
-        Route::group('Menu',[
-            'index'=> [
-                'admin/Menu/index',
-                ['method' => 'get']
-            ],
-            'changeStatus' =>[
-                'admin/Menu/changeStatus',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/Menu/add',
-                ['method' => 'post']
-            ],
-            'edit' =>[
-                'admin/Menu/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/Menu/del',
-                ['method' => 'get']
-            ]
-        ]);
-        Route::group('User',[
-            'index'=> [
-                'admin/User/index',
-                ['method' => 'get']
-            ],
-            'getUsers' =>[
-                'admin/User/getUsers',
-                ['method' => 'get']
-            ],
-            'changeStatus' =>[
-                'admin/User/changeStatus',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/User/add',
-                ['method' => 'post']
-            ],
-            'own' =>[
-                'admin/User/own',
-                ['method' => 'post']
-            ],
-            'edit' =>[
-                'admin/User/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/User/del',
-                ['method' => 'get']
-            ],
-        ]);
-        Route::group('Auth',[
-            'index'=> [
-                'admin/Auth/index',
-                ['method' => 'get']
-            ],
-            'changeStatus' =>[
-                'admin/Auth/changeStatus',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/Auth/add',
-                ['method' => 'post']
-            ],
-            'delMember' =>[
-                'admin/Auth/delMember',
-                ['method' => 'get']
-            ],
-            'edit' =>[
-                'admin/Auth/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/Auth/del',
-                ['method' => 'get']
-            ],
-            'getGroups' =>[
-                'admin/Auth/getGroups',
-                ['method' => 'get']
-            ],
-            'getRuleList' =>[
-                'admin/Auth/getRuleList',
-                ['method' => 'get']
-            ]
-        ]);
-        Route::group('App',[
-            'index'=> [
-                'admin/App/index',
-                ['method' => 'get']
-            ],
-            'refreshAppSecret'=> [
-                'admin/App/refreshAppSecret',
-                ['method' => 'get']
-            ],
-            'changeStatus' =>[
-                'admin/App/changeStatus',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/App/add',
-                ['method' => 'post']
-            ],
-            'getAppInfo' =>[
-                'admin/App/getAppInfo',
-                ['method' => 'get']
-            ],
-            'edit' =>[
-                'admin/App/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/App/del',
-                ['method' => 'get']
-            ]
-        ]);
-        Route::group('InterfaceList',[
-            'index'=> [
-                'admin/InterfaceList/index',
-                ['method' => 'get']
-            ],
-            'changeStatus' =>[
-                'admin/InterfaceList/changeStatus',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/InterfaceList/add',
-                ['method' => 'post']
-            ],
-            'refresh' =>[
-                'admin/InterfaceList/refresh',
-                ['method' => 'get']
-            ],
-            'edit' =>[
-                'admin/InterfaceList/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/InterfaceList/del',
-                ['method' => 'get']
-            ],
-            'getHash' =>[
-                'admin/InterfaceList/getHash',
-                ['method' => 'get']
-            ]
-        ]);
-        Route::group('Fields',[
-            'index'=> [
-                'admin/Fields/index',
-                ['method' => 'get']
-            ],
-            'request' =>[
-                'admin/Fields/request',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/Fields/add',
-                ['method' => 'post']
-            ],
-            'response' =>[
-                'admin/Fields/response',
-                ['method' => 'get']
-            ],
-            'edit' =>[
-                'admin/Fields/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/Fields/del',
-                ['method' => 'get']
-            ],
-            'upload' =>[
-                'admin/Fields/upload',
-                ['method' => 'post']
-            ]
-        ]);
-        Route::group('InterfaceGroup',[
-            'index'=> [
-                'admin/InterfaceGroup/index',
-                ['method' => 'get']
-            ],
-            'getAll' =>[
-                'admin/InterfaceGroup/getAll',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/InterfaceGroup/add',
-                ['method' => 'post']
-            ],
-            'changeStatus' =>[
-                'admin/InterfaceGroup/changeStatus',
-                ['method' => 'get']
-            ],
-            'edit' =>[
-                'admin/InterfaceGroup/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/InterfaceGroup/del',
-                ['method' => 'get']
-            ]
-        ]);
-        Route::group('AppGroup',[
-            'index'=> [
-                'admin/AppGroup/index',
-                ['method' => 'get']
-            ],
-            'getAll' =>[
-                'admin/AppGroup/getAll',
-                ['method' => 'get']
-            ],
-            'add' =>[
-                'admin/AppGroup/add',
-                ['method' => 'post']
-            ],
-            'changeStatus' =>[
-                'admin/AppGroup/changeStatus',
-                ['method' => 'get']
-            ],
-            'edit' =>[
-                'admin/AppGroup/edit',
-                ['method' => 'post']
-            ],
-            'del' =>[
-                'admin/AppGroup/del',
-                ['method' => 'get']
-            ]
-        ]);
-        Route::group('Log',[
-            'index'=> [
-                'admin/Log/index',
-                ['method' => 'get']
-            ],
-            'del' =>[
-                'admin/Log/del',
-                ['method' => 'get']
-            ]
-        ]);
-        Route::miss('admin/Miss/index');
-    },['after_behavior' => $afterBehavior]
+Route::group('admin', function() {
+    Route::group('Menu', [
+        'index'        => [
+            'admin/Menu/index',
+            ['method' => 'get']
+        ],
+        'changeStatus' => [
+            'admin/Menu/changeStatus',
+            ['method' => 'get']
+        ],
+        'add'          => [
+            'admin/Menu/add',
+            ['method' => 'post']
+        ],
+        'edit'         => [
+            'admin/Menu/edit',
+            ['method' => 'post']
+        ],
+        'del'          => [
+            'admin/Menu/del',
+            ['method' => 'get']
+        ]
+    ]);
+    Route::group('User', [
+        'index'        => [
+            'admin/User/index',
+            ['method' => 'get']
+        ],
+        'getUsers'     => [
+            'admin/User/getUsers',
+            ['method' => 'get']
+        ],
+        'changeStatus' => [
+            'admin/User/changeStatus',
+            ['method' => 'get']
+        ],
+        'add'          => [
+            'admin/User/add',
+            ['method' => 'post']
+        ],
+        'own'          => [
+            'admin/User/own',
+            ['method' => 'post']
+        ],
+        'edit'         => [
+            'admin/User/edit',
+            ['method' => 'post']
+        ],
+        'del'          => [
+            'admin/User/del',
+            ['method' => 'get']
+        ],
+    ]);
+    Route::group('Auth', [
+        'index'        => [
+            'admin/Auth/index',
+            ['method' => 'get']
+        ],
+        'changeStatus' => [
+            'admin/Auth/changeStatus',
+            ['method' => 'get']
+        ],
+        'add'          => [
+            'admin/Auth/add',
+            ['method' => 'post']
+        ],
+        'delMember'    => [
+            'admin/Auth/delMember',
+            ['method' => 'get']
+        ],
+        'edit'         => [
+            'admin/Auth/edit',
+            ['method' => 'post']
+        ],
+        'del'          => [
+            'admin/Auth/del',
+            ['method' => 'get']
+        ],
+        'getGroups'    => [
+            'admin/Auth/getGroups',
+            ['method' => 'get']
+        ],
+        'getRuleList'  => [
+            'admin/Auth/getRuleList',
+            ['method' => 'get']
+        ]
+    ]);
+    Route::group('App', [
+        'index'            => [
+            'admin/App/index',
+            ['method' => 'get']
+        ],
+        'refreshAppSecret' => [
+            'admin/App/refreshAppSecret',
+            ['method' => 'get']
+        ],
+        'changeStatus'     => [
+            'admin/App/changeStatus',
+            ['method' => 'get']
+        ],
+        'add'              => [
+            'admin/App/add',
+            ['method' => 'post']
+        ],
+        'getAppInfo'       => [
+            'admin/App/getAppInfo',
+            ['method' => 'get']
+        ],
+        'edit'             => [
+            'admin/App/edit',
+            ['method' => 'post']
+        ],
+        'del'              => [
+            'admin/App/del',
+            ['method' => 'get']
+        ]
+    ]);
+    Route::group('InterfaceList', [
+        'index'        => [
+            'admin/InterfaceList/index',
+            ['method' => 'get']
+        ],
+        'changeStatus' => [
+            'admin/InterfaceList/changeStatus',
+            ['method' => 'get']
+        ],
+        'add'          => [
+            'admin/InterfaceList/add',
+            ['method' => 'post']
+        ],
+        'refresh'      => [
+            'admin/InterfaceList/refresh',
+            ['method' => 'get']
+        ],
+        'edit'         => [
+            'admin/InterfaceList/edit',
+            ['method' => 'post']
+        ],
+        'del'          => [
+            'admin/InterfaceList/del',
+            ['method' => 'get']
+        ],
+        'getHash'      => [
+            'admin/InterfaceList/getHash',
+            ['method' => 'get']
+        ]
+    ]);
+    Route::group('Fields', [
+        'index'    => [
+            'admin/Fields/index',
+            ['method' => 'get']
+        ],
+        'request'  => [
+            'admin/Fields/request',
+            ['method' => 'get']
+        ],
+        'add'      => [
+            'admin/Fields/add',
+            ['method' => 'post']
+        ],
+        'response' => [
+            'admin/Fields/response',
+            ['method' => 'get']
+        ],
+        'edit'     => [
+            'admin/Fields/edit',
+            ['method' => 'post']
+        ],
+        'del'      => [
+            'admin/Fields/del',
+            ['method' => 'get']
+        ],
+        'upload'   => [
+            'admin/Fields/upload',
+            ['method' => 'post']
+        ]
+    ]);
+    Route::group('InterfaceGroup', [
+        'index'        => [
+            'admin/InterfaceGroup/index',
+            ['method' => 'get']
+        ],
+        'getAll'       => [
+            'admin/InterfaceGroup/getAll',
+            ['method' => 'get']
+        ],
+        'add'          => [
+            'admin/InterfaceGroup/add',
+            ['method' => 'post']
+        ],
+        'changeStatus' => [
+            'admin/InterfaceGroup/changeStatus',
+            ['method' => 'get']
+        ],
+        'edit'         => [
+            'admin/InterfaceGroup/edit',
+            ['method' => 'post']
+        ],
+        'del'          => [
+            'admin/InterfaceGroup/del',
+            ['method' => 'get']
+        ]
+    ]);
+    Route::group('AppGroup', [
+        'index'        => [
+            'admin/AppGroup/index',
+            ['method' => 'get']
+        ],
+        'getAll'       => [
+            'admin/AppGroup/getAll',
+            ['method' => 'get']
+        ],
+        'add'          => [
+            'admin/AppGroup/add',
+            ['method' => 'post']
+        ],
+        'changeStatus' => [
+            'admin/AppGroup/changeStatus',
+            ['method' => 'get']
+        ],
+        'edit'         => [
+            'admin/AppGroup/edit',
+            ['method' => 'post']
+        ],
+        'del'          => [
+            'admin/AppGroup/del',
+            ['method' => 'get']
+        ]
+    ]);
+    Route::group('Log', [
+        'index' => [
+            'admin/Log/index',
+            ['method' => 'get']
+        ],
+        'del'   => [
+            'admin/Log/del',
+            ['method' => 'get']
+        ]
+    ]);
+    Route::miss('admin/Miss/index');
+}, ['after_behavior' => $afterBehavior]
 );
