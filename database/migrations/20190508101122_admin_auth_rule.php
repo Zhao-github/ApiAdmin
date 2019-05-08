b1ece57b691ef8ab9cf1289a893c9ebafaa222c2
zhaoxiang <zhaoxiang051405@gmail.com>
2019-05-08 18:28:18 +0800

    modified 添加数据库迁移脚本

--- database/migrations/20190508101122_admin_auth_rule.php
+++ database/migrations/20190508101122_admin_auth_rule.php
@@ -3,7 +3,7 @@
 use think\migration\Migrator;
 use think\migration\db\Column;
 
-class AdminAuthGroupAccess extends Migrator {
+class AdminAuthRule extends Migrator {
     /**
      * Change Method.
      *
@@ -26,19 +26,28 @@ class AdminAuthGroupAccess extends Migrator {
      * with the Table class.
      */
     public function change() {
-        $table = $this->table('admin_auth_group_access', [
-            'comment' => '用户和组的对应关系'
+        $table = $this->table('admin_auth_rule', [
+            'comment' => '权限细节'
         ])->setCollation('utf8mb4_general_ci');
-        $table->addColumn('uid', 'integer', [
+        $table->addColumn('url', 'string', [
+            'limit'   => 80,
+            'default' => '',
+            'comment' => '规则唯一标识'
+        ])->addColumn('groupId', 'integer', [
             'limit'   => 11,
             'default' => 0,
             'signed'  => false,
-            'comment' => ''
-        ])->addColumn('groupId', 'string', [
-            'limit'   => 255,
-            'default' => '',
-            'comment' => ''
-        ])->addIndex(['uid'])->addIndex(['groupId'])->create();
+            'comment' => '权限所属组的ID'
+        ])->addColumn('auth', 'integer', [
+            'limit'   => 11,
+            'default' => 0,
+            'signed'  => false,
+            'comment' => '权限数值'
+        ])->addColumn('status', 'integer', [
+            'limit'   => 1,
+            'default' => 1,
+            'comment' => '状态：为1正常，为0禁用'
+        ])->create();
 
         $table->changeColumn('id', 'integer', ['signed' => false]);
     }
