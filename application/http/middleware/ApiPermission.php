db76f641f54190a43bd3a9aac8cfc216cc4d5758
zhaoxiang <756958008@qq.com>
2019-07-01 00:06:50 +0800

    modified 完成接口鉴权

--- application/http/middleware/ApiPermission.php
+++ application/http/middleware/ApiPermission.php
@@ -2,6 +2,8 @@
 
 namespace app\http\middleware;
 
+use app\util\ReturnCode;
+
 class ApiPermission {
 
     /**
@@ -12,8 +14,18 @@ class ApiPermission {
      * @author zhaoxiang <zhaoxiang051405@gmail.com>
      */
     public function handle($request, \Closure $next) {
+        $header = config('apiadmin.CROSS_DOMAIN');
         $appInfo = $request->APP_CONF_DETAIL;
         $apiInfo = $request->API_CONF_DETAIL;
+
+        $allRules = explode(',', $appInfo['app_api']);
+        if (!in_array($apiInfo['hash'], $allRules)) {
+            return json([
+                'code' => ReturnCode::INVALID,
+                'msg'  => '非常抱歉，您没有权限这么做！',
+                'data' => []
+            ])->header($header);
+        }
 
         return $next($request);
     }
