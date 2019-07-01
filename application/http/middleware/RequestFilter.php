2b1f817f5c0984e4127db9e85c9cb60d98d50b66
zhaoxiang <zhaoxiang051405@gmail.com>
2019-07-01 11:40:05 +0800

    modified 优化手机号正则

--- application/http/middleware/RequestFilter.php
+++ application/http/middleware/RequestFilter.php
@@ -114,7 +114,7 @@ class RequestFilter {
                         }
                         break;
                     case DataType::TYPE_MOBILE:
-                        $newRule[$value['field_name'] . '|' . $value['info']]['regex'] = '/^1[34578]\d{9}$/';
+                        $newRule[$value['field_name'] . '|' . $value['info']]['regex'] = '/^1[345789]\d{9}$/';
                         break;
                 }
             }
