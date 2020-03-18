f93ce20eb078ca3a1bd442576a5b9aa96c08ea88
zhaoxiang <zhaoxiang051405@gmail.com>
2020-03-18 16:54:17 +0800

    modified 添加数据库迁移

---
+++ database/migrations/20200318085203_change_admin_menu_field.php
@@ -0,0 +1,35 @@
+<?php
+
+use think\migration\Migrator;
+
+class ChangeAdminMenuField extends Migrator {
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up() {
+        $this->table('admin_menu')->renameColumn('name', 'title')->save();
+    }
+
+    public function down() {
+        $this->table('admin_menu')->renameColumn('title', 'name')->save();
+    }
+
+}
