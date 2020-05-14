bea766191d2ee2517a766cd977d7fa51d029e940
zhaoxiang <zhaoxiang051405@gmail.com>
2020-05-14 20:06:52 +0800

    modified 添加相关工具类

---
+++ application/util/RouterTool.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @since   2020-05-14
+ * @author  zhaoxiang <zhaoxiang051405@gmail.com>
+ */
+
+namespace app\util;
+
+
+class RouterTool {
+    
+    public static function buildAdminRouter() {
+        
+    }
+
+    public static function buildVueRouter() {
+        
+    }
+}
