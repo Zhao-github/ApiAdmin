de2e1903f1ef24ce70c3b3b79944fd6f494b5312
zhaoxiang <zhaoxiang051405@gmail.com>
2020-09-29 11:27:30 +0800

    add 添加wiki部分路由

---
+++ route/wikiRoute.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Wiki路由
+ * @since   2019-08-12
+ * @author  zhaoxiang <zhaoxiang051405@gmail.com>
+ */
+
+use think\facade\Route;
+
+Route::group('wiki', function() {
+    Route::rule(
+        'Api/login', 'wiki/Api/login', 'post'
+    );
+    Route::group('Api', function() {
+        Route::rule('login', 'wiki/Api/login', 'post');
+        Route::rule('errorCode', 'wiki/Api/errorCode', 'get');
+        Route::rule('groupList', 'wiki/Api/groupList', 'get');
+        Route::rule('detail', 'wiki/Api/detail', 'get');
+        Route::rule('logout', 'wiki/Api/logout', 'get');
+    })->middleware(['WikiAuth']);
+
+    //MISS路由定义
+    Route::miss('admin/Miss/index');
+})->middleware('AdminResponse');
