f2229738759515f3260942aa1b1ee39bacfe6d65
zhaoxiang <zhaoxiang051405@gmail.com>
2020-09-29 17:11:38 +0800

    modified 修改model文件全部为严格模式

--- app/model/AdminAuthGroup.php
+++ app/model/AdminAuthGroup.php
@@ -1,15 +1,17 @@
 <?php
+declare (strict_types=1);
 /**
  *
  * @since   2018-02-08
  * @author  zhaoxiang <zhaoxiang051405@gmail.com>
  */
-
 namespace app\model;
+
+use think\model\relation\HasMany;
 
 class AdminAuthGroup extends Base {
 
-    public function rules() {
+    public function rules(): HasMany {
         return $this->hasMany('AdminAuthRule', 'group_id', 'id');
     }
 }
--- app/model/AdminUser.php
+++ app/model/AdminUser.php
@@ -1,4 +1,5 @@
 <?php
+declare (strict_types=1);
 /**
  * @since   2017-11-02
  * @author  zhaoxiang <zhaoxiang051405@gmail.com>
@@ -6,11 +7,13 @@
 
 namespace app\model;
 
+use think\model\relation\HasOne;
+
 class AdminUser extends Base {
 
     protected $autoWriteTimestamp = true;
 
-    public function userData() {
+    public function userData(): HasOne {
         return $this->hasOne('AdminUserData', 'uid', 'id');
     }
 }
